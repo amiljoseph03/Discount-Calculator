bade563a1002037cc0cb3972297b58be3226990f
amiljoseph03 <amiljoseph03@gmail.com>
2025-10-27 09:14:43 +0530

    input added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Discount calculator</h1>
+      <div className="calculator">
+        <h1>Discount Calculator</h1>
+        <div className="inputs">
+          <input type="text" name="" id="" placeholder="Enter amount" />
+        </div>
+        <div className="input2">
+          <input
+            type="number"
+            name=""
+            id=""
+            placeholder="Enter discount percentage"
+          />
+        </div>
+        <button>Calculate</button>
+
+        <div className="result">
+          <h3>Amount you saved : </h3>
+          <h3>Final price : </h3>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
